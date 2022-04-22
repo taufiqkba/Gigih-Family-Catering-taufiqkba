0018ae7176efd5fe2c2621877cbda77813b388fc
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2022-04-22 15:28:57 +0000

    Add migration for Menu model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_22_013423) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_22_152000) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
