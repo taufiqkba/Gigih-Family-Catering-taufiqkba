3c934d01638ea2cb4586c2394cf77cbdc0c4b509
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2022-04-22 01:54:00 +0000

    Delete existing db, and remigration for testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_21_163717) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_22_013423) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
