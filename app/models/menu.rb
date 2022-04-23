a0f6aaa5346e0583d14536bb77d544d98af440fc
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2022-04-23 08:04:23 +0000

    add relation between menu and category model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,9 @@
 class Menu < ApplicationRecord
+  #relation
+  has_many :categories_menus
+	has_many :categories, through: :categories_menus
 
-
+  #validation
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true, length: { maximum: 150, too_long: "%{count} characters is the maximum allowed" }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
