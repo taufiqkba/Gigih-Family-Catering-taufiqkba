f737a2195719e12f9385855fa3511e2ae01a69a2
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2022-04-23 08:03:38 +0000

    Add relation between category and model menu

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ApplicationRecord
+  #relation
+  has_many :categories_menus
+	has_many :menus, through: :categories_menus
 
-  
+  #validation
   validates :name, presence: true, uniqueness: true
 end
